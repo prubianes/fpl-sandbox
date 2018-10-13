e60b040f23b91b1a025bd5d7cfc8791cd975e95d
Pablo Rubianes <pablorubianes@gmail.com>
2018-10-13 01:26:00 -0300

    Get request for positions

--- request-data.py
+++ request-data.py
@@ -1,8 +1,9 @@
 import requests
 
+url_base = 'https://fantasy.premierleague.com/drf/bootstrap-static'
+
 def get_players_info():
-    url = 'https://fantasy.premierleague.com/drf/bootstrap-static'
-    r = requests.get(url)
+    r = requests.get(url_base)
     for player in r.json()['elements']:
         print('---------------- \r')
         print(player['element_type'])
@@ -10,6 +11,12 @@ def get_players_info():
         print(player['team_code'])
         print(player['first_name'] + ' ' + player['second_name'])
         print(player['now_cost'])
+
+def get_positions():
+    r = requests.get(url)
+    for positions in r.json()['element_types']:
+        print(positions['singular_name'])
+
 
 if __name__ == '__main__':
     get_players_info()
