fbaf108be820c4dd956cb3143ef5e0a240c5c801
Pablo Rubianes <pablorubianes@gmail.com>
2018-10-16 01:13:43 -0300

    more requests methods

--- request-data.py
+++ request-data.py
@@ -1,6 +1,7 @@
 import requests
 
 url_base = 'https://fantasy.premierleague.com/drf/bootstrap-static'
+url_base_player_detail = 'https://fantasy.premierleague.com/drf/element-summary/'
 
 def get_players_info():
     r = requests.get(url_base)
@@ -13,9 +14,20 @@ def get_players_info():
         print(player['now_cost'])
 
 def get_positions():
-    r = requests.get(url)
+    r = requests.get(url_base)
     for positions in r.json()['element_types']:
         print(positions['singular_name'])
+
+def get_teams():
+    r = requests.get(url_base)
+    for team in r.json()['teams']:
+        print(team['name'])
+
+def get_players_details(id_element):
+    r = requests.get(url_base_player_detail + id_element)
+    # we have to see what data is gonna be used
+    for history in r.json['history_past']:
+        print(history['season_name'])
 
 
 if __name__ == '__main__':
